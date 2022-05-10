664e57c1907230b9576591cfe2ef40e78cb042f4
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-05-10 16:12:30 +0300

    Update replace_in_code.ts

--- src/compiler/preprocess/replace_in_code.ts
+++ src/compiler/preprocess/replace_in_code.ts
@@ -45,27 +45,6 @@ function calculate_replacements(
 }
 
 function perform_replacements(
-	replacements: Replacement[],
-	source: Source
-): MappedCode {
-	const out = new MappedCode();
-	let last_end = 0;
-
-	for (const { offset, length, replacement } of replacements) {
-		const unchanged_prefix = MappedCode.from_source(
-			slice_source(source.source.slice(last_end, offset), last_end, source)
-		);
-		out.concat(unchanged_prefix).concat(replacement);
-		last_end = offset + length;
-	}
-
-	const unchanged_suffix = MappedCode.from_source(slice_source(source.source.slice(last_end), last_end, source));
-
-	return out.concat(unchanged_suffix);
-}
-
-export async function replace_in_code(
-	regex: RegExp,
 	get_replacement: (...match: any[]) => Promise<MappedCode>,
 	location: Source
 ): Promise<MappedCode> {
